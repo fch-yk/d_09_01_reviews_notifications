1f30bbd1772557e352d304ecc741129c33d9dd49
fch-yk <yefim_korshever@mail.ru>
2023-01-06 11:32:45 +0300

    Send review message

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pprint
 import time
 
 import requests
+import telegram
 from environs import Env
 
 logger = logging.getLogger(__file__)
@@ -15,6 +16,9 @@ def main():
     env = Env()
     env.read_env()
     devman_token = env("DEVMAN_TOKEN")
+    bot_token = env("REVIEW_BOT_TOKEN")
+    chat_id = env("TELEGRAM_USER_ID")
+    bot = telegram.Bot(token=bot_token)
     url = "https://dvmn.org/api/long_polling/"
     headers = {"Authorization": f"Token {devman_token}"}
     timestamp = None
@@ -33,6 +37,11 @@ def main():
             response_card = response.json()
             timestamp = response_card["last_attempt_timestamp"]
             pprint.pprint(response_card)
+            if response_card["status"] == "found":
+                bot.send_message(
+                    text="The teacher reviewed your work!",
+                    chat_id=chat_id
+                )
             logger.debug("last_attempt_timestamp: %s", timestamp)
         except requests.exceptions.ReadTimeout:
             logger.debug("Response timeout.")
